8491ead67aa58e6ef5db1f0dc6cb3bc218dd2eab
wenop <wenoptics@163.com>
2015-10-28 13:16:00 +0800

    update 增加了下拉刷新； 增加了搜索地区按钮； 增加了侧滑目录； 解决contentImageView的显示问题； 解决主框的多余padding；

---
+++ app/src/main/java/tk/wenop/testapp/UI/SideActivity.java
@@ -0,0 +1,182 @@
+package tk.wenop.testapp.UI;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import java.util.ArrayList;
+
+import tk.wenop.testapp.Adapter.MainScreenChatAdapter;
+import tk.wenop.testapp.Overview.MainScreenOverviewItem;
+import tk.wenop.testapp.R;
+
+public class SideActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    private MainScreenChatAdapter mainActRVAdapter;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.LayoutManager mRVLayoutM;
+
+    protected ArrayList<MainScreenOverviewItem> mainActDataSet;
+
+    // 下拉刷新
+    SwipeRefreshLayout mSwipeRefreshLayout;
+
+    void refreshItems() {
+        // Load items
+        //TODO llwoll
+
+        // Load complete
+        onItemsLoadComplete();
+    }
+
+    void onItemsLoadComplete() {
+        // Update the adapter and notify data set changed
+        //TODO llwoll
+
+        // Stop refresh animation
+        mSwipeRefreshLayout.setRefreshing(false);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_side);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("天津市");
+
+        setSupportActionBar(toolbar);
+
+        // 下拉刷新
+        mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.main_screen_swipe_refresh);
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                refreshItems();
+            }
+        });
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                // TODO wenop
+                //  在此处允许用户发内容
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        //设置recyclerView
+        mRecyclerView = (RecyclerView) findViewById(R.id.mainscreen_recycler_view);
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        mRVLayoutM = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mRVLayoutM);
+
+
+        // wenop-test
+        //   测试增加几个item... 看看效果
+        mainActDataSet = new ArrayList<MainScreenOverviewItem>();
+        MainScreenOverviewItem msovi_0 = new MainScreenOverviewItem();
+        msovi_0.mNickName = "wenop";
+        msovi_0.mLocation = "天津市";
+        msovi_0.mCommentCount = 11;
+        mainActDataSet.add(msovi_0);
+        MainScreenOverviewItem msovi_1 = new MainScreenOverviewItem();
+        msovi_1.mNickName = "wenop_002";
+        msovi_1.mLocation = "天津市";
+        msovi_1.mCommentCount = 2;
+        mainActDataSet.add(msovi_1);
+
+        // specify an adapter
+        mainActRVAdapter = new MainScreenChatAdapter(mainActDataSet);
+        mRecyclerView.setAdapter(mainActRVAdapter);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_actionbar_main_screen, menu);
+
+        // Associate searchable configuration with the SearchView
+        SearchView searchView =
+                (SearchView) menu.findItem(R.id.search_place).getActionView();
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camara) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
